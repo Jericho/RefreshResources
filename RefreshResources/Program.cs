e43bc721af9b16cd8861199e096a5fcc7a555e6a
Jeremie Desautels <desautelsj@hotmail.com>
2019-09-22 14:17:16 -0400

    Root is now on drive D

--- RefreshResources/Program.cs
+++ RefreshResources/Program.cs
@@ -20,7 +20,7 @@ namespace RefreshResources
 {
 	class Program
 	{
-		private const string ROOT_FOLDER = "C:\\_build\\";
+		private const string ROOT_FOLDER = "D:\\_build\\";
 		private const string SOURCE_FOLDER = ROOT_FOLDER + "resources";
 		private const int MAX_NUGET_CONCURENCY = 25; // 25 seems like a safe value but I suspect that nuget allows a much large number of concurrent connections.
 
