db2194abf0e1a307f1f1ec00aeeef6ce2583ec66
Jericho <desautelsj@hotmail.com>
2022-04-15 10:31:49 -0400

    Add .cake folder to .gitignore

--- RefreshResources/Program.cs
+++ RefreshResources/Program.cs
@@ -178,7 +178,7 @@ namespace RefreshResources
 				content = content
 					.Trim('\n')
 					.Replace("# Created by https://www.toptal.com/developers/gitignore/api/visualstudio", "# Created with the help of https://www.toptal.com/developers/gitignore/api/visualstudio (formerly https://www.gitignore.io/api/visualstudio)")
-					.Replace("# Cake - Uncomment if you are using it\n# tools/**\n# !tools/packages.config", "# Cake\ntools/**\nBuildArtifacts/")
+					.Replace("# Cake - Uncomment if you are using it\n# tools/**\n# !tools/packages.config", "# Cake\n.cake/**\ntools/**\nBuildArtifacts/")
 					.Replace("# End of https://www.toptal.com/developers/gitignore/api/visualstudio", "# WinMerge\n*.bak\n\n# End of https://www.toptal.com/developers/gitignore/api/visualstudio")
 					.Replace("\n", Environment.NewLine);
 
